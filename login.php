c0e2551824f2c0f84853aca6dbd7b26a2c9171a5
Lohit Chandu <lohitchandu.7@gmail.com>
2020-05-17 16:11:12 +0530

    my project

---
+++ login.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PROCESSING A FORM</title>
+</head>
+<body>
+	<h1>WELCOME</h1>
+	<p>Your Username is</p>
+	<?php echo $_POST["Username"]; ?>
+	<p>Your password id secured</p>
+    <?php echo $_POST["password"]; ?>
+
+</body>
+</html>
